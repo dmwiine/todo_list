d9174d4426c53cbbc10fe985b8293799cc43dd11
Donna Mwiine <dmwiine@gmail.com>
2019-02-17 23:40:33 +0300

    implement authorization api request

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AuthorizeApiRequest do
-    let!(:user){ create(:user) }
+    let(:user){ create(:user) }
     # Mock `Authorization` header
     let(:header) { { 'Authorization' => token_generator(user.id) } }
     # Invalid request subject
