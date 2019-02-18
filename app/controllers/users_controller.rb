4109d291633188046d352685d02634a20e2cd70c
Donna Mwiine <dmwiine@gmail.com>
2019-02-18 10:07:50 +0300

    add user signup implementation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,21 @@
 class UsersController < ApplicationController
-    before_action :set_todo, only: [:update, :destroy, :show]
-    # GET /users
-    def index
-      @users = User.all
-      json_response @users
-    end
-  
-    # POST /users
-    def create
-      @user = User.create!(user_params)
-      json_response @user, :created
-    end
-  
-    # GET /users/:id
-    def show
-      json_response @user
-    end
-  
-    # PUT /users/:id
-    def update
-      @user = User.update(user_params)
-      json_response @user, :updated
-    end
-  
-    # DELETE /users/:id
-    def destroy
-      @user.destroy
-      head :no_content
-    end
-    private
-  
-    def user_params
-      params.permit(:username, :email, :password_digest)
-    end
-  
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # POST /signup
+  # return authenticated token upon signup
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end
